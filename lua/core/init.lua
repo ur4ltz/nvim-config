d762f7ef93b1b8c8682cf9b96a3ecded6aa1d359
Andy Shevchenko <ur4ltz@gmail.com>
2021-04-15 16:57:18 +0300

    Added keymaps.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,5 +1,6 @@
 -- vim: ts=2 sw=2 et:
 
 require('core.options')
+require('core.keymaps')
 require('plugins')
 
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,11 @@
+-- vim: ts=2 sw=2 et:
+
+require('util.keys')
+local g = vim.g
+
+g.mapleader      = ' '
+g.maplocalleader = ','
+
+nnoremap('<Leader>w', ':w<CR>')
+nnoremap('<Leader>q', ':q<CR>')
+
