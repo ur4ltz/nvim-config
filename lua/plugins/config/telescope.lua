9ce7444fd6b9ec6fb4bceb3c15472c70ed02e7fe
Andy Shevchenko <ur4ltz@gmail.com>
2021-05-17 17:23:00 +0300

    Align text.

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,6 +1,6 @@
 -- vim: ts=2 sw=2 et:
 
-local actions = require('telescope.actions')
+local actions   = require('telescope.actions')
 local telescope = require('telescope')
 
 telescope.setup {
@@ -14,14 +14,14 @@ telescope.setup {
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
-    prompt_prefix = "> ",
-    selection_caret = "> ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
+    prompt_position    = "bottom",
+    prompt_prefix      = "> ",
+    selection_caret    = "> ",
+    entry_prefix       = "  ",
+    initial_mode       = "insert",
     selection_strategy = "reset",
-    sorting_strategy = "descending",
-    layout_strategy = "horizontal",
+    sorting_strategy   = "descending",
+    layout_strategy    = "horizontal",
     layout_defaults = {
       horizontal = {
         mirror = false,
@@ -30,23 +30,23 @@ telescope.setup {
         mirror = false,
       },
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_sorter          = require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    shorten_path = true,
-    winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
-    border = {},
-    borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-    color_devicons = true,
-    use_less = true,
-    set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    generic_sorter       = require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path         = true,
+    winblend             = 0,
+    width                = 0.75,
+    preview_cutoff       = 120,
+    results_height       = 1,
+    results_width        = 0.8,
+    border               = {},
+    borderchars          = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    color_devicons       = true,
+    use_less             = true,
+    set_env              = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+    file_previewer       = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer       = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer     = require'telescope.previewers'.vim_buffer_qflist.new,
 
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
